1a2dcdbaf1d314620b0512ce32e27dbcc6916e73
Steve Purcell <steve@sanityinc.com>
2018-10-25 16:24:43 +1300

    Fix broken parens

--- ibuffer-vc.el
+++ ibuffer-vc.el
@@ -130,8 +130,8 @@ If the file is not under version control, nil is returned instead."
                    ((memq backend '(cvs CVS)) (vc-find-root file-name "CVS"))
                    ((memq backend '(rcs RCS)) (or (vc-find-root file-name "RCS")
                                                   (concat file-name ",v")))
-                   ((memq backend '(src SRC) (or (vc-find-root file-name ".src")
-                                                 (concat file-name ",v"))))
+                   ((memq backend '(src SRC)) (or (vc-find-root file-name ".src")
+                                                  (concat file-name ",v")))
                    (t (error "ibuffer-vc: don't know how to find root for vc backend '%s' - please submit a bug report or patch" backend)))))
             (cons backend root-dir)))))))
 
